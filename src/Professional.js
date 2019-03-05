fefef63578b09e2ee839fe4848a4ec77a1a457f8
Rick Toews <rick.toews@gmail.com>
2019-03-04 17:07:15 -0800

    Use general-content style for text on Professional page.

--- src/Professional.js
+++ src/Professional.js
@@ -55,7 +55,7 @@ class Professional extends Component {
               <article>
 			  <h2>Linux</h2>
               <h3>Linux find</h3>
-			  <Typography variant="body1" gutterBottom>The idea here is to suppress any "permission denied" items that one might otherwise see in the output of the find command. A way I recently learned to do that:</Typography>
+			  <p className="general-content">The idea here is to suppress any "permission denied" items that one might otherwise see in the output of the find command. A way I recently learned to do that:</p>
 			  <SyntaxHighlighter language='javascript' style={docco} wrapLines={true}>
 			  find ./ -name [filename] 2>/dev/null
 			  </SyntaxHighlighter>
@@ -64,20 +64,20 @@ class Professional extends Component {
               <article>
 			  <h2>JavaScript</h2>
               <h3>JavaScript sort numeric</h3>
-			  <Typography variant="body1" gutterBottom>Not long ago, I ran into a situation in which I found that numbers weren't being sorted as numbers but as if they were strings. So, for example, 10 would appear before 4, because the character '1' precedes the character '4' in a string sort. I learned that Array.prototype.sort() is, indeed, documented as behaving this way: "The default sort order is built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values."</Typography>
+			  <p className="general-content">Not long ago, I ran into a situation in which I found that numbers weren't being sorted as numbers but as if they were strings. So, for example, 10 would appear before 4, because the character '1' precedes the character '4' in a string sort. I learned that Array.prototype.sort() is, indeed, documented as behaving this way: "The default sort order is built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values."</p>
 			  <Typography variant="body1" gutterBottom>A simple way to deal perform a numeric sort:</Typography>
 			  <SyntaxHighlighter language='javascript' style={docco} wrapLines={true}>
 			  arr.sort((a, b) => a-b);
 			  </SyntaxHighlighter>
-			  <Typography variant="body1" gutterBottom>The subtraction forces the use of numeric values, and (a-b) returns a positive number if a > b, and, therefore, their relative positions in the array need to be switched.</Typography>
+			  <p className="general-content">The subtraction forces the use of numeric values, and (a-b) returns a positive number if a > b, and, therefore, their relative positions in the array need to be switched.</p>
               </article>
 
 			  <article>
 			  <h2>JavaScript</h2>
 			  <h3>Generators</h3>
-			  <Typography variant="body1" gutterBottom>I recent started playing with generators in JavaScript. The concept was a new for me, so assimilation wasn't instant; however, it wasn't long before I find myself wanting to solve a problem for which a generator seemed just the thing.</Typography>
-			  <Typography variant="body1" gutterBottom>I was playing with the concept of the primitive Pythagorean triple (that is, a Pythagorean triple whose elements have a GCF of 1) and wanted to be able to get the "next" square from a list of odd squares, beginning at a certain point and continuing as long as needed.</Typography>
-			  <Typography variant="body1" gutterBottom>The code looked like this:</Typography>
+			  <p className="general-content">I recent started playing with generators in JavaScript. The concept was a new for me, so assimilation wasn't instant; however, it wasn't long before I find myself wanting to solve a problem for which a generator seemed just the thing.</p>
+			  <p className="general-content">I was playing with the concept of the primitive Pythagorean triple (that is, a Pythagorean triple whose elements have a GCF of 1) and wanted to be able to get the "next" square from a list of odd squares, beginning at a certain point and continuing as long as needed.</p>
+			  <p className="general-content">The code looked like this:</p>
 			  <SyntaxHighlighter language='javascript' style={docco} wrapLines={true}>
 			  {generatorCode}
 			  </SyntaxHighlighter>
