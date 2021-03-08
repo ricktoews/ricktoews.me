6f0c8dd5611dbf97300f6fc937074d663a2cc62b
Rick Toews <rick.toews@gmail.com>
2021-03-07 21:57:48 -0800

    Set multiple choice flags.

--- src/components/GeoGame.js
+++ src/components/GeoGame.js
@@ -25,6 +25,8 @@ function GeoGame(props) {
 	const [ flagList, setFlagList ] = useState([]);
 	const [ randomCountry, setRandomCountry ] = useState();
 	const [ challengePath, setChallengePath ] = useState();
+	const [ bordering, setBordering ] = useState([]);
+	const [ correctBordering, setCorrectBordering ] = useState([]);
 
 	useEffect(async () => {
 		var res = await fetch(geoFlagsUrl);
@@ -35,11 +37,36 @@ function GeoGame(props) {
 	}, []);
 
 	/* Pick a path from those identified. */
-	useEffect(() => {
+	useEffect(async () => {
 		if (path.length > 0) {
 			var pathNdx = Math.floor(Math.random() * path.length);
+			var randPath = path[pathNdx];
+			var dest = randPath[randPath.length - 1];
+
+
+			var url = geoCrossingsUrl + `${origin}/1`;
+			var res = await fetch(url);
+			var res = await res.json();
+			var data = res.data || res;
+			var origBordering = data.paths.map(path => path[1]);
+//			console.log('bordering countries for origin', origBordering);
+
+			var url = geoCrossingsUrl + `${dest}/1`;
+			var res = await fetch(url);
+			var res = await res.json();
+			var data = res.data || res;
+			var destBordering = data.paths.map(path => path[1]);
+//			console.log('bordering countries for destination', destBordering);
+
+
+			var validPaths = path.filter(p => p[p.length - 1] === dest);
+			var correctCountries = validPaths.map(path => path.slice(1, path.length - 1)).flat();
+//			console.log('bordering countries', correctBordering);
+			var bordering = [...origBordering, ...destBordering, ...correctCountries];
+			console.log('multiple choice countries', bordering);
 			setChallengePath(path[pathNdx]);
-			console.log('Challenge path', path[pathNdx]);
+			setBordering(bordering);
+			setCorrectBordering(correctCountries);
 		}
 
 	}, [path]);
@@ -61,6 +88,7 @@ function GeoGame(props) {
 	}, [origin, crossings ]);
 
 	/* Origin or Destination has changed, so get list of border crossings between the countries. */
+	/*
 	useEffect(async () => {
 		if (origin && destination) {
 			var url = geoUrl + `${origin}/${destination}`;
@@ -75,6 +103,7 @@ function GeoGame(props) {
 			}
 		}
 	}, [origin, destination ]);
+	*/
 
 	const pickOne = () => {
 		var rndNdx = Math.floor(Math.random() * flagList.length);
@@ -141,10 +170,16 @@ function GeoGame(props) {
 	`;
 
 	const FlagSelectionGrid = props => {
+		var borderingCountries = JSON.stringify(bordering);
 		if (flagList) {
+			var multipleChoiceFlags = flagList.filter(flag => {
+				let isBordering = borderingCountries.indexOf(flag.countryName) !== -1;
+				return isBordering;
+			});
+
 			return (
 				<FlagGrid>
-				{ flagList.map((flag, key) => {
+				{ multipleChoiceFlags.map((flag, key) => {
 					return <div key={key} onClick={handleClickedFlag} data-country={flag.countryName}><img src={flag.flagUrl} /></div>
 				}) }
 				</FlagGrid>
@@ -156,7 +191,11 @@ function GeoGame(props) {
 	const handleClickedFlag = e => {
 		e.preventDefault();
 		var el = e.currentTarget;
-		console.log('clicked', el.dataset.country);
+		var selectedCountry = el.dataset.country;
+		if (correctBordering.indexOf(selectedCountry) !== -1) {
+			console.log('Correct!', selectedCountry, correctBordering);
+		}
+		console.log('clicked', correctBordering);
 	}
 
 	const handleSetOrigin = e => {
