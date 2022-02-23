6b2f8e8717d4b54a061eed20b3ba3f782d9f04ba
Rick Toews <rick.toews@gmail.com>
2022-02-22 23:48:49 -0800

    Force rebuild.

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ function withNav(MyComponent, title, showFilter = false) {
 };
 
 function App(props) {
-	console.log('Testing deploy (5).');
 	const HomeContent = ({ categoryFilter }) => {
 		console.log('HomeContent filter', categoryFilter);
 		return (
