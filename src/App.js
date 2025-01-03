c4bd70ba1e5b1f413489ed1de2aedd8496966642
Rick Toews <rick.toews@gmail.com>
2025-01-02 23:41:24 -0800

    New github.io page, is the idea.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import Navbar from "./Navbar";
 import Home from "./Home";
-import About from "./About";
-import Articles from "./Articles";
+import Navbar from "./Navbar"
 import "./assets/css/App.css";
 
 function App(props) {
-  const { content } = props;
-  const categories = Array.from(
-    new Set(content.map((item) => item.category))
-  ).sort();
-
+  const categories = [];
   return (
     <div className="App">
-      <Navbar categories={categories} />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="/category/:category"
-          element={<Articles content={content} />}
-        />
       </Routes>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -1,82 +1,93 @@
 import { useEffect, useRef, useState } from 'react';
 import "./assets/css/App.css";
 
-const places = [
-  {
-    classSuffix: 'oceanario',
-    location: 'Océanario Lisboa',
-    city: 'Lisbon, Portugal'
-  },
-  {
-    classSuffix: 'westminster',
-    location: 'Westminster Abbey',
-    city: 'London, England'
-  },
-  {
-    classSuffix: 'beaufort-castle',
-    location: 'Beaufort Castle',
-    city: 'Beaufort, Luxembourg'
-  },
-  {
-    classSuffix: 'alaska-cruise',
-    location: 'Cruise Scenery',
-    city: 'Alaska, USA'
-  }
-];
-console.log(`Test deploy, since the revert apparently didn't trigger it.`);
 
 const Home = (props, state) => {
-  const [ slide, setSlide ] = useState(0);
+  const [portfolioItem, setPortfolioItem] = useState('wordmage');
 
-  const updateSlide = () => {
-    console.log('slide', slide);
+  const handlePortfolioClick = e => {
+    const el = e.currentTarget;
+    const item = el.id;
+    setPortfolioItem(item);
+  }
 
-    if (slide < places.length - 1) {
-      setSlide(slide + 1);
-    } 
-    else {
-      setSlide(0);
+  const showPortfolioSite = e => {
+    let url;
+    if (portfolioItem === 'wordmage') {
+      url = 'https://wordmage.app';
     }
-  };
+    else if (portfolioItem === 'calendar') {
+      url = 'https://calendar.math-toys.app';
+    }
+    else if (portfolioItem === 'art') {
+      url = 'https://art-game.vercel.app';
+    }
+    if (url) {
+      var windowName = "popup";
+      var windowFeatures = "width=600,height=450,scrollbars=yes";
 
+      // Open the window
+      var newWindow = window.open(url, windowName, windowFeatures);
 
-  useEffect(() => {
-    let t = setTimeout(updateSlide, 5000);
-    return () => clearTimeout(t);
-  }, [slide]);
+      if (newWindow) {
+        // Center the window
+        newWindow.moveTo(
+          window.screenX + (window.outerWidth / 2) - (newWindow.outerWidth / 2),
+          window.screenY + (window.outerHeight / 2) - (newWindow.outerHeight / 2)
+        );
+
+        // Set content (replace this with your actual content or URL)
+        newWindow.document.close();
+      }
+    }
+  }
 
   return (
-    <div className="home-container">
-      <div className="app-upper-background-container">
-        <div className="app-upper-background-picture-container">
-          <div className={`places-transition app-upper-background-picture app-upper-background-picture-${places[slide].classSuffix}`}></div>
-        </div>
+    <div className="container">
+      <div className="header">
+        <h1>Rick's Pad</h1>
       </div>
-
-      <div className="app-lower-background-container">
-        <div className="app-lower-background-picture-container">
-          <div className={`places-transition app-lower-background-picture app-lower-background-picture-${places[slide].classSuffix}`}></div>
+      <div className="content">
+        <div className="left-content">
+          <div className="portfolio-link" id="wordmage" onClick={handlePortfolioClick}>Wordmage.app</div>
+          <div className="portfolio-link" id="calendar" onClick={handlePortfolioClick}>calendar.math-toys.app</div>
+          <div className="portfolio-link" id="art" onClick={handlePortfolioClick}>art-game.vercel.app</div>
         </div>
+        <div className="divider"></div>
+        <div className="right-content">
+          <div className="scrollable-content">
+            <div className={"portfolio-content " + (portfolioItem === 'wordmage' ? 'active' : '')}>
+              <h2>WordMage</h2>
+              <div onClick={showPortfolioSite} className="portfolio-link"><img className="screenshot" src="/images/screenshot-wordmage.png" /></div>
 
-        <div className="app-picture-label-container">
-          <div className="description">{places[slide].location}</div>
-          <div className="location">{places[slide].city}</div>
-        </div>
-      </div>
+              <p>This reflects my lifelong interest in words--especially those likely to be a bit off the beaten path. The app has a built-in list of several thousand words, which I scraped from a list that someone else made and apparently abandoned. (My source was not the original one.) The user can add new words and also mark words as of no interest, so that they don't remain in active display.</p>
 
-      <div className="home-content">
-        <div className="title">ricktoews.me</div>
-        <div className="tag-line">
-          🇵🇹🇭🇷🇳🇱🇧🇪🇱🇺🇨🇿🇦🇹🇩🇰🇫🇷🇮🇸🇬🇧
-          <br />
-          Recreational Traveler
-          <br />
-          Software Developer
-          <br />
-          T: @retorick
+              <p>The landing page of the app shows a random list of words, intentionally unsorted, to give the user something quick and unpredictable to browse. A Refresh button allows the user to get a new selection.</p>
+
+              <p>Each word has a set of buttons (learn, like, meh, tag) to allow the user to organize it in some way. Words marked as “learn” are added to the pool from which the Unscramble area of the app makes its selections.</p>
+
+              <p>A user's customizations are saved in the browser's local storage. However, there is the option to register and thus set up a profile that's accessed via email and password. If a user chooses to register, the customizations are stored in a database and so can be accessed from a different browser.</p>
+            </div>
+
+            <div onClick={showPortfolioSite} className={"portfolio-content " + (portfolioItem === 'calendar' ? 'active' : '')}>
+              <h2>Calendar</h2>
+              <div onClick={showPortfolioSite} className="portfolio-link"><img className="screenshot" src="/images/screenshot-calendar.png" /></div>
+
+              <p>Long ago, a math enthusiast showed me how to find the day of the week for a given date, using a 12-digit calendar. I took the concept and developed it further so I could directly calculate the calendar for any year. This app provides a large list of 12-digit calendars and also gives instructions for performing the calculations.</p>
+
+            </div>
+            <div onClick={showPortfolioSite} className={"portfolio-content " + (portfolioItem === 'art' ? 'active' : '')}>
+              <h2>Art Game</h2>
+              <div onClick={showPortfolioSite} className="portfolio-link"><img className="screenshot" src="/images/screenshot-art.png" /></div>
+
+              <p>This is an art gallery game. The purpose is to help the user become familiar with paintings considered to be among the great works of art. I think the idea came to me while I was chatting with my brother about identifying music from a passage within it. My idea was to make a game that would show the user a cutout of a painting and challenge the user to identify the painting based on the cutout.</p>
+
+            </div>
+          </div>
         </div>
       </div>
     </div>
+
   );
 };
 
